cf4756c1427cd290873762984c2d6efc3a86d9f7
ASCSagar <ascsagar08@gmail.com>
2024-03-26 18:22:23 +0530

    FlashCard-26-03

--- src/components/Dashboard/Student/FlashCard/FlashCard.js
+++ src/components/Dashboard/Student/FlashCard/FlashCard.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "../../../../css/student panel/fleshcards.css";
-import { useLocation } from "react-router-dom";
-import DSSidebar from "../DSSideBar/DSSideBar";
 import ajaxCall from "../../../../helpers/ajaxCall";
 import FlashCardModal from "./FlashCardModal";
 
-const FlashCard = () => {
-  const { state: { enrolledCourse } = {} } = useLocation();
+const FlashCard = ({ courseId }) => {
   const [flashCardList, setFlashCardList] = useState([]);
   const [isFlipped, setIsFlipped] = useState({});
   const [modalShow, setModalShow] = useState(false);
@@ -31,8 +28,8 @@ const FlashCard = () => {
         );
         if (response?.status === 200) {
           setFlashCardList(
-            response?.data.filter((item) =>
-              enrolledCourse?.some((course) => course?.id === item.course?.id)
+            response?.data.filter(
+              (item) => item?.course?.id === parseInt(courseId)
             )
           );
 
@@ -49,7 +46,7 @@ const FlashCard = () => {
         console.log("error", error);
       }
     })();
-  }, [enrolledCourse]);
+  }, [courseId]);
 
   const handleCloseModal = () => {
     setModalShow(false);
@@ -57,89 +54,63 @@ const FlashCard = () => {
   };
 
   return (
-    <div className="body__wrapper">
-      <div className="main_wrapper overflow-hidden">
-        <div className="dashboardarea sp_bottom_100">
-          <div className="dashboard">
-            <div className="container-fluid full__width__padding">
-              <div className="row">
-                <DSSidebar />
-                <div className="col-xl-9 col-lg-9 col-md-12">
-                  <div className="dashboard__content__wraper common-background-color-across-app">
-                    <div className="dashboard__section__title">
-                      <h4>Flash Cards</h4>
-                    </div>
-                    <div className="row">
-                      {flashCardList &&
-                        flashCardList.map(
-                          ({
-                            id,
-                            description,
-                            set_priority,
-                            course,
-                            flash_card_items,
-                          }) => (
-                            <div
-                              key={id}
-                              className="col-xl-4 col-lg-6 col-md-12 col-sm-6 col-12"
-                              data-aos="fade-up"
-                            >
-                              <div
-                                className="gridarea__wraper gridarea__wraper__2 global-neomorphism-card-styling"
-                                onClick={() => setModalShow(true)}
-                              >
-                                <div className="gridarea__content">
-                                  <div className="gridarea__heading">
-                                    <h3>{course?.Course_Title}</h3>
-                                  </div>
-                                  <div className="zoom__meeting__id">
-                                    <p>
-                                      Description :{" "}
-                                      <span
-                                        style={{
-                                          color: "#01579b",
-                                          fontWeight: "700",
-                                        }}
-                                      >
-                                        {description}
-                                      </span>
-                                    </p>
-                                  </div>
-                                  <div className="zoom__meeting__id">
-                                    <p>
-                                      Priority :{" "}
-                                      <span
-                                        style={{
-                                          color: "#01579b",
-                                          fontWeight: "700",
-                                        }}
-                                      >
-                                        {set_priority}
-                                      </span>
-                                    </p>
-                                  </div>
-                                </div>
-                              </div>
-                              <FlashCardModal
-                                show={modalShow}
-                                onHide={handleCloseModal}
-                                flash_card_items={flash_card_items}
-                                isFlipped={isFlipped}
-                                setIsFlipped={setIsFlipped}
-                                currentCardIndex={currentCardIndex}
-                                setCurrentCardIndex={setCurrentCardIndex}
-                              />
-                            </div>
-                          )
-                        )}
-                    </div>
+    <div className="row">
+      {flashCardList &&
+        flashCardList.map(
+          ({ id, description, set_priority, course, flash_card_items }) => (
+            <div
+              key={id}
+              className="col-xl-4 col-lg-6 col-md-12 col-sm-6 col-12"
+              data-aos="fade-up"
+            >
+              <div
+                className="gridarea__wraper gridarea__wraper__2 global-neomorphism-card-styling"
+                onClick={() => setModalShow(true)}
+              >
+                <div className="gridarea__content">
+                  <div className="gridarea__heading">
+                    <h3>{course?.Course_Title}</h3>
+                  </div>
+                  <div className="zoom__meeting__id">
+                    <p>
+                      Description :{" "}
+                      <span
+                        style={{
+                          color: "#01579b",
+                          fontWeight: "700",
+                        }}
+                      >
+                        {description}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="zoom__meeting__id">
+                    <p>
+                      Priority :{" "}
+                      <span
+                        style={{
+                          color: "#01579b",
+                          fontWeight: "700",
+                        }}
+                      >
+                        {set_priority}
+                      </span>
+                    </p>
                   </div>
                 </div>
               </div>
+              <FlashCardModal
+                show={modalShow}
+                onHide={handleCloseModal}
+                flash_card_items={flash_card_items}
+                isFlipped={isFlipped}
+                setIsFlipped={setIsFlipped}
+                currentCardIndex={currentCardIndex}
+                setCurrentCardIndex={setCurrentCardIndex}
+              />
             </div>
-          </div>
-        </div>
-      </div>
+          )
+        )}
     </div>
   );
 };

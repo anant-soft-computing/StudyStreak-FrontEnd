c89122613ccb9b09fe82d8bf3162723276e2eac3
nishith003 <nishithshah03@gmail.com>
2024-07-15 12:24:29 +0530

    fix: pagination start from 1

--- src/components/LiveExam/FullLengthLiveTest.js
+++ src/components/LiveExam/FullLengthLiveTest.js
@@ -422,10 +422,10 @@ const FullLengthLiveExam = () => {
           <audio
             controls
             autoPlay
-            controlsList="nodownload noplaybackrate noplay"
-            className="hidden-controls"
+            controlsList='nodownload noplaybackrate noplay'
+            className='hidden-controls'
           >
-            <source src={audio_file} type="audio/mpeg" />
+            <source src={audio_file} type='audio/mpeg' />
           </audio>
         </div>
       );
@@ -439,11 +439,11 @@ const FullLengthLiveExam = () => {
     return (
       <>
         {image && (
-          <div className="text-center">
+          <div className='text-center'>
             <img
-              className="mb-2"
+              className='mb-2'
               src={image}
-              alt="Study Streak"
+              alt='Study Streak'
               height={250}
               width={250}
             />
@@ -969,14 +969,14 @@ const FullLengthLiveExam = () => {
     examAnswer.map((test, index) => (
       <div key={index}>
         <h4>Test : {index + 1}</h4>
-        <div className="card-container">
+        <div className='card-container'>
           {test.data.map((answer, idx) => (
-            <div key={idx} className="card answer__width">
-              <div className="card-body">
-                <h6 className="card-title">Q. {idx + 1}</h6>
-                <h6 className="card-text">
+            <div key={idx} className='card answer__width'>
+              <div className='card-body'>
+                <h6 className='card-title'>Q. {idx + 1}</h6>
+                <h6 className='card-text'>
                   Answer :{" "}
-                  <span className="text-success">{answer.answer_text}</span>
+                  <span className='text-success'>{answer.answer_text}</span>
                 </h6>
               </div>
             </div>
@@ -990,50 +990,49 @@ const FullLengthLiveExam = () => {
       return null;
     }
     let tempQuestionNumber = 0;
-    return uniqueIdArr?.map((item, sectionIndex) => {
-      if (item.examType !== examData.exam_type) {
-        item?.paginationsIds?.map(() => {
-          tempQuestionNumber = tempQuestionNumber + 1;
-        });
-        return null;
-      }
-      return (
-        <div className="lv-section" key={item.name + sectionIndex}>
-          {/* Section name */}
-          <button
-            className="lv-footer-section"
-            onClick={() => setNext(sectionIndex)}
-          >
-            {item.name}
-          </button>
-          {/* Section pagination */}
-          {item.paginationsIds?.map((pagination, paginationIndex) => {
-            tempQuestionNumber = tempQuestionNumber + 1;
-            return (
-              <div
-                className={`lv-footer-item ${
-                  examAnswer[sectionIndex] &&
-                  examAnswer[sectionIndex].data.length > 0 &&
-                  examAnswer[sectionIndex].data[paginationIndex].answer_text !==
-                    ""
-                    ? "lv-completed-questions"
-                    : ""
-                }`}
-                onClick={() => {
-                  if (next !== sectionIndex) setNext(sectionIndex);
-                  setTimeout(() => {
-                    scrollToContent(pagination, sectionIndex);
-                  }, 100);
-                }}
-                key={pagination + paginationIndex}
-              >
-                {tempQuestionNumber}
-              </div>
-            );
-          })}
-        </div>
-      );
-    });
+    return uniqueIdArr
+      ?.filter((item) => item?.examType === examData?.exam_type)
+      ?.map((item, sectionIndex) => {
+        // if (item.examType !== examData.exam_type) {
+        //   return null;
+        // }
+        return (
+          <div className='lv-section' key={item.name + sectionIndex}>
+            {/* Section name */}
+            <button
+              className='lv-footer-section'
+              onClick={() => setNext(sectionIndex)}
+            >
+              {`section-${sectionIndex + 1}`}
+            </button>
+            {/* Section pagination */}
+            {item.paginationsIds?.map((pagination, paginationIndex) => {
+              tempQuestionNumber = tempQuestionNumber + 1;
+              return (
+                <div
+                  className={`lv-footer-item ${
+                    examAnswer[sectionIndex] &&
+                    examAnswer[sectionIndex].data.length > 0 &&
+                    examAnswer[sectionIndex].data[paginationIndex]
+                      ?.answer_text !== ""
+                      ? "lv-completed-questions"
+                      : ""
+                  }`}
+                  onClick={() => {
+                    if (next !== sectionIndex) setNext(sectionIndex);
+                    setTimeout(() => {
+                      scrollToContent(pagination, sectionIndex);
+                    }, 100);
+                  }}
+                  key={pagination + paginationIndex}
+                >
+                  {tempQuestionNumber}
+                </div>
+              );
+            })}
+          </div>
+        );
+      });
   }, [uniqueIdArr, examAnswer, next, examData]);
 
   const recorderContainer = useCallback(
@@ -1059,7 +1058,7 @@ const FullLengthLiveExam = () => {
   );
 
   return instructionCompleted.showInstruction ? (
-    <div className="test-instruction">
+    <div className='test-instruction'>
       {instructionCompleted.type.reading === 1 && (
         <>
           <SmallModal
@@ -1069,7 +1068,7 @@ const FullLengthLiveExam = () => {
             Thank You For Complete Test
           </SmallModal>
           <ReadingInstruction
-            testType="Full Length"
+            testType='Full Length'
             startTest={handleInstruction}
           />
         </>
@@ -1083,14 +1082,14 @@ const FullLengthLiveExam = () => {
             Thank You For Complete Test
           </SmallModal>
           <WritingInstruction
-            testType="Full Length"
+            testType='Full Length'
             startTest={handleInstruction}
           />
         </>
       )}
       {instructionCompleted.type.listening === 1 && (
         <ListeningInstruction
-          testType="Full Length"
+          testType='Full Length'
           startTest={handleInstruction}
         />
       )}
@@ -1103,7 +1102,7 @@ const FullLengthLiveExam = () => {
             Thank You For Complete Test
           </SmallModal>
           <SpeakingInstruction
-            testType="Full Length"
+            testType='Full Length'
             startTest={handleInstruction}
           />
         </>
@@ -1112,47 +1111,47 @@ const FullLengthLiveExam = () => {
   ) : (
     <>
       {/* Navbar */}
-      <div className="lv-navbar lv-navbar-responsive">
-        <div className="lv-navbar-title">
+      <div className='lv-navbar lv-navbar-responsive'>
+        <div className='lv-navbar-title'>
           <h2>{examData?.exam_category}</h2>
-          <div className="lv-userName">{userData?.username}</div>
+          <div className='lv-userName'>{userData?.username}</div>
           <div style={{ margin: "15px 0px 0 10px" }}>/</div>
-          <div className="lv-userName">{`${examData?.exam_name}`}</div>
+          <div className='lv-userName'>{`${examData?.exam_name}`}</div>
         </div>
-        <span className="lv-navbar-title">
-          Time Taken :<span className="lv-userName">{timeTaken}</span>
+        <span className='lv-navbar-title'>
+          Time Taken :<span className='lv-userName'>{timeTaken}</span>
         </span>
-        <div className="lv-navbar-title-mobile">
-          <div className="username-mobile">
+        <div className='lv-navbar-title-mobile'>
+          <div className='username-mobile'>
             <h2>{examData?.exam_category}</h2>
-            <div className="mobile-breadcumb">
-              <div className="lv-userName">{userData?.username}</div>
+            <div className='mobile-breadcumb'>
+              <div className='lv-userName'>{userData?.username}</div>
               <div style={{ margin: "15px 0px 0 10px" }}>/</div>
-              <div className="lv-userName">{`${examData?.exam_name}`}</div>
+              <div className='lv-userName'>{`${examData?.exam_name}`}</div>
             </div>
           </div>
-          <div className="lv-navbar-footer">
+          <div className='lv-navbar-footer'>
             <span>
-              Time Taken :<span className="lv-userName">{timeTaken}</span>
+              Time Taken :<span className='lv-userName'>{timeTaken}</span>
             </span>
           </div>
         </div>
       </div>
-      <div className="lv-container">
+      <div className='lv-container'>
         {/* Main Container */}
         {examData?.exam_type === "Listening" &&
           renderAudio(examData?.audio_file)}
-        <div className="lv-main-container">
+        <div className='lv-main-container'>
           {/* Left Container */}
           {(examData?.exam_type === "Reading" ||
             examData?.exam_type === "Writing") && (
-            <div className="lv-left-container">
+            <div className='lv-left-container'>
               {displayLeftContainer(examData?.passage, examData?.passage_image)}
             </div>
           )}
           {examData?.exam_type === "Speaking" && (
             <div
-              className="lv-left-container"
+              className='lv-left-container'
               style={{
                 display: "flex",
                 flexDirection: "column",
@@ -1165,11 +1164,11 @@ const FullLengthLiveExam = () => {
                     (element) => element.id === item.id
                   );
                   return (
-                    <div className="lv-question-container">
-                      <div className="d-flex align-items-center lv-btn-mic-container">
+                    <div className='lv-question-container'>
+                      <div className='d-flex align-items-center lv-btn-mic-container'>
                         {i + 1} :
                         <button
-                          className="lv-footer-button lv-speaking-button"
+                          className='lv-footer-button lv-speaking-button'
                           onClick={() => speak(item.question, item.id)}
                           disabled={
                             examAnswer[next].data?.[speakingIndex]?.status === 1
@@ -1205,11 +1204,11 @@ const FullLengthLiveExam = () => {
             examData?.exam_type === "Listening" ||
             examData?.exam_type === "Writing") && (
             <div
-              className="lv-right-container"
-              id="right-container"
+              className='lv-right-container'
+              id='right-container'
               ref={containerRef}
             >
-              <div className="lv-box-right">
+              <div className='lv-box-right'>
                 {/* Replace the following with your actual content */}
                 {(examData?.exam_type === "Reading" ||
                   examData?.exam_type === "Listening") && (
@@ -1220,10 +1219,10 @@ const FullLengthLiveExam = () => {
                   />
                 )}
                 {examData?.exam_type === "Writing" && (
-                  <div className="lv-textarea">
+                  <div className='lv-textarea'>
                     <textarea
                       id={`textarea_${next}`}
-                      className="writing__textarea"
+                      className='writing__textarea'
                       value={examAnswer[next]?.data?.[0]?.answer_text || ""}
                       onChange={(e) => handleWritingAnswer(e, next)}
                     />
@@ -1235,22 +1234,22 @@ const FullLengthLiveExam = () => {
             </div>
           )}
         </div>
-        <div className="d-flex justify-content-between align-items-center mb-3 mt-2 flex-column flex-md-row">
-          <div className="lv-question-pagination d-flex justify-content-between align-items-center pb-1 w-100 mb-2 mb-md-0">
-            <div className="lv-section-pagination">{renderPagination}</div>
+        <div className='d-flex justify-content-between align-items-center mb-3 mt-2 flex-column flex-md-row'>
+          <div className='lv-question-pagination d-flex justify-content-between align-items-center pb-1 w-100 mb-2 mb-md-0'>
+            <div className='lv-section-pagination'>{renderPagination}</div>
           </div>
-          <div className="lv-footer-btn pb-1">
+          <div className='lv-footer-btn pb-1'>
             {(examData?.exam_type === "Reading" ||
               examData?.exam_type === "Listening") && (
               <button
-                className="lv-footer-button review_size"
+                className='lv-footer-button review_size'
                 onClick={() => setIsModalOpen(true)}
               >
                 Reviews
               </button>
             )}
             <button
-              className="lv-footer-button"
+              className='lv-footer-button'
               style={{
                 display: next === 0 ? "none" : "block",
               }}
@@ -1259,7 +1258,7 @@ const FullLengthLiveExam = () => {
               <span>Back</span>
             </button>
             <button
-              className="lv-footer-button"
+              className='lv-footer-button'
               style={{
                 display: fullPaper.length === next + 1 ? "none" : "block",
               }}
@@ -1268,7 +1267,7 @@ const FullLengthLiveExam = () => {
               <span>&#10152;</span>
             </button>
             <button
-              className="lv-footer-button"
+              className='lv-footer-button'
               style={{
                 display: next !== (fullPaper.length > 0 ? "none" : "block"),
               }}
@@ -1280,16 +1279,16 @@ const FullLengthLiveExam = () => {
         </div>
         {isConfirmModalOpen && (
           <SmallModal
-            size="lg"
+            size='lg'
             centered
             isOpen={isConfirmModalOpen}
             footer={
-              <div className="d-flex gap-2">
-                <button className="btn btn-success" onClick={handleRLSubmit}>
+              <div className='d-flex gap-2'>
+                <button className='btn btn-success' onClick={handleRLSubmit}>
                   Yes
                 </button>
                 <button
-                  className="btn btn-danger"
+                  className='btn btn-danger'
                   onClick={() => setIsConfirmModalOpen(false)}
                 >
                   No
@@ -1305,9 +1304,9 @@ const FullLengthLiveExam = () => {
           (examData?.exam_type === "Reading" ||
             examData?.exam_type === "Listening") && (
             <SmallModal
-              size="lg"
+              size='lg'
               centered
-              title="Your Answers"
+              title='Your Answers'
               isOpen={isModalOpen}
               onClose={() => setIsModalOpen(false)}
             >

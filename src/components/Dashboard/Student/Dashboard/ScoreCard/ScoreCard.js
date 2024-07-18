8f1e59ba2fac86833e75c9b9f391c88224e5315d
ASCSagar <ascsagar08@gmail.com>
2024-07-18 19:53:46 +0530

    Remove ScoreCard API Integration

--- src/components/Dashboard/Student/Dashboard/ScoreCard/ScoreCard.js
+++ src/components/Dashboard/Student/Dashboard/ScoreCard/ScoreCard.js
@@ -1,48 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import ajaxCall from "../../../../../helpers/ajaxCall";
 
 const ScoreCard = () => {
-  const [fltData, setFltData] = useState({});
-  const [practiceTestData, setPracticeTestData] = useState({});
-
-  const fetchTestData = async (url, setData) => {
-    try {
-      const response = await ajaxCall(
-        url,
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${
-              JSON.parse(localStorage.getItem("loginInfo"))?.accessToken
-            }`,
-          },
-          method: "GET",
-        },
-        8000
-      );
-      if (response.status === 200) {
-        setData(response.data[0]);
-      } else {
-        console.error("Error fetching data");
-      }
-    } catch (error) {
-      console.error("Error fetching data", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTestData(
-      "/test-submission/?test_type=practise_set&records=1",
-      setPracticeTestData
-    );
-  }, []);
-
-  useEffect(() => {
-    fetchTestData("/test-submission/?test_type=flt&records=1", setFltData);
-  }, []);
-
   return (
     <>
       <div className="col-xl-6 column__custom__class">
@@ -52,22 +11,11 @@ const ScoreCard = () => {
               <h6>Your Latest Given Practice Test</h6>
             </div>
             <div className="gridarea__price d-flex gap-2 mb-0">
-              <h3>
-                {practiceTestData?.practise_set_name} -{" "}
-                {practiceTestData?.practise_set_type}
-              </h3>
+              <h3>Reading Practice Test 1 - Reading</h3>
             </div>
             <div className="gridarea__bottom">
               <div className="gridarea__small__content">
-                <Link
-                  to={
-                    practiceTestData.practise_set_type === "Writing" ||
-                    practiceTestData.practise_set_type === "Speaking"
-                      ? `/practice-assessment/${practiceTestData?.practise_set}`
-                      : `/exam-practice-test-answer/${practiceTestData?.practise_set}`
-                  }
-                  state={{ examType: practiceTestData?.practise_set_type }}
-                >
+                <Link to="">
                   <h6>View Full Report {">>"}</h6>
                 </Link>
               </div>
@@ -82,7 +30,7 @@ const ScoreCard = () => {
               <h6>Your Latest Full Length Test </h6>
             </div>
             <div className="gridarea__price d-flex gap-2 mb-0">
-              <h3>{fltData?.flt_set_name}</h3>
+              <h3>Full Length Test - 1</h3>
             </div>
             <div className="gridarea__bottom">
               <div className="gridarea__small__content">
